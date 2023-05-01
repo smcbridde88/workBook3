8766aa8a4a4c091c405c107a996a51bc01b87a94
Sasha McBride <mcbride.sasha@gmail.com>
2023-05-01 11:37:58 -0400

    initial

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,14 @@
+function favoriteThings(){
+    console.log("Sasha's favorite things are: ");
+    console.log("My favorite movie is Friday After Next");
+    console.log("My favorite band is plain white tees");
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log(`i want to learn a new skill and make more money`)
+
+}
+
+whyImHere();
